19088aac3753fa08025e8f196bd914e69f46f88d
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-07-16 13:54:33 +0300

    feat: repository was created

---
+++ FootballApp/FootballManager.Repository/Common/Repository.cs
@@ -0,0 +1,47 @@
+﻿using FootballApp.Core.Context;
+using FootballApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager.Repository.Common
+{
+    public class Repository<TEntity, TKey> : IRepository<TEntity, TKey>
+       where TEntity : class, IEntity<TKey>
+    {
+        protected ProjectContext _ctx;
+
+        public Repository(ProjectContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync() => await _ctx.Set<TEntity>().ToListAsync();
+
+        public virtual async Task CreateAsync(TEntity entity)
+        {
+            await _ctx.Set<TEntity>().AddAsync(entity);
+            await SaveAsync();
+        }
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            _ctx.Set<TEntity>().Update(entity);
+            await SaveAsync();
+        }
+        public virtual async Task DeleteAsync(TKey id)
+        {
+            _ctx.Set<TEntity>().Remove(await _ctx.Set<TEntity>().FindAsync(id));
+            await SaveAsync();
+
+        }
+        public virtual async Task<TEntity> GetAsync(TKey id)
+        {
+            return await _ctx.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task SaveAsync() => await _ctx.SaveChangesAsync();
+    }
+}
